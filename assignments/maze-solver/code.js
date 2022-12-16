358e2ebafad32c9d4904502ca86d2217e65b38a3
ion <111394893+ionisea@users.noreply.github.com>
2022-12-16 12:16:40 -0800

    Updating assignments/maze-solver/code.js

--- assignments/maze-solver/code.js
+++ assignments/maze-solver/code.js
@@ -1,6 +1,6 @@
 const rotate = (cx, cy, x, y, angle) => {
-    let radians = (Math.PI / 180) * angle;
-    cos = Math.cos(radians),
+    let radians = (Math.PI / 180) * angle,
+     cos = Math.cos(radians),
         sin = Math.sin(radians),
         nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
         ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
