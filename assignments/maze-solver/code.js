e0d06de763d346796c14873eecbbfb55a155f835
ion <111394893+ionisea@users.noreply.github.com>
2022-12-26 14:38:39 -0800

    Updating assignments/maze-solver/code.js

--- assignments/maze-solver/code.js
+++ assignments/maze-solver/code.js
@@ -5,6 +5,7 @@ const drawnCircle = (coordArray) => {
   if (coordArray.length == 2){
     const radius = Math.hypot(Math.abs(coordArray[0].x-coordArray[1].x),Math.abs(coordArray[0].y-coordArray[1].y))
     drawCircle(coordArray[0].x, coordArray[0].y, radius, 'white')
+    CircleCoords = []
   }
 }
 
