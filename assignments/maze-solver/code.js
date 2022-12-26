024883888e53385d9714d9e33f5dc335bfc0d224
ion <111394893+ionisea@users.noreply.github.com>
2022-12-26 14:51:03 -0800

    Updating assignments/maze-solver/code.js

--- assignments/maze-solver/code.js
+++ assignments/maze-solver/code.js
@@ -1,11 +1,18 @@
 drawFilledRect(0, 0, width, height, 'black')
+const Density = 100 // measured in kg/pixel
 const ObjArray = []
 let CircleCoords = []
+class Shape{
+  constructor(radius, activeForce, coordinates){
+    this.mass = 
+  }
+}
+
 const drawnCircle = (coordArray) => {
   if (coordArray.length == 2){
     const radius = Math.hypot(Math.abs(coordArray[0].x-coordArray[1].x),Math.abs(coordArray[0].y-coordArray[1].y))
     drawCircle(coordArray[0].x, coordArray[0].y, radius, 'white')
-    drawLine(coordArray[0].x, coordArray[0].y, coordArray[1].x, coordArray[1].y, 1, 'white')
+    //drawLine(coordArray[0].x, coordArray[0].y, coordArray[1].x, coordArray[1].y, 1, 'white')
     CircleCoords = []
   }
 }
