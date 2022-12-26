94b16cd6f40b664b8e557fbb984ac1b3fd8bdcbd
ion <111394893+ionisea@users.noreply.github.com>
2022-12-26 15:44:08 -0800

    Updating assignments/maze-solver/code.js

--- assignments/maze-solver/code.js
+++ assignments/maze-solver/code.js
@@ -4,7 +4,7 @@ const radToDeg = (degAngle) => degAngle * 180 / Math.PI
 
 //vectors
 const vector = (angle, magnitude) => {
-  return ({ angle: angle, magnitude })
+  return ({ angle: angle * Math.PI / 180, magnitude })
 }
 
 const add2Vectors = (a) => {
@@ -12,7 +12,7 @@ const add2Vectors = (a) => {
   const x2 = Math.cos(a[1].angle) * a[1].magnitude
   const y1 = Math.sin(a[0].angle) * a[0].magnitude
   const y2 = Math.sin(a[1].angle) * a[1].magnitude
-  const angle = Math.atan2(y1 + y2, x1 + x2)
+  const angle = Math.atan2(y1 + y2, x1 + x2) * 180 / Math.PI
   const mag = Math.sqrt((x1 + x2) ** 2 + (y1 + y2) ** 2)
   return ({ angle, magnitude: mag })
 }
