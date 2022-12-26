70a4d533e4cc8cf4ffb1329e2d0aaf807321a847
ion <111394893+ionisea@users.noreply.github.com>
2022-12-26 15:09:52 -0800

    Updating assignments/maze-solver/code.js

--- assignments/maze-solver/code.js
+++ assignments/maze-solver/code.js
@@ -1,7 +1,32 @@
+//vectors
+const vector = (angle, magnitude) => {
+  return ({ angle: angle * Math.PI / 180, magnitude })
+}
+
+const add2Vectors = (a) => {
+  const x1 = Math.cos(a[0].angle) * a[0].magnitude
+  const x2 = Math.cos(a[1].angle) * a[1].magnitude
+  const y1 = Math.sin(a[0].angle) * a[0].magnitude
+  const y2 = Math.sin(a[1].angle) * a[1].magnitude
+  const angle = Math.atan2(y1 + y2, x1 + x2)
+  const mag = Math.sqrt((x1 + x2) ** 2 + (y1 + y2) ** 2)
+  return ({ angle, magnitude: mag })
+}
+
+const vectorMultiply = (o, n) => {
+  if (n >= 0) {
+    return ({ angle: o.angle, magnitude: o.magnitude * n })
+  } else {
+    return ({ angle: o.angle + Math.PI, magnitude: o.magnitude * -n })
+  }
+}
+
+//global
 drawFilledRect(0, 0, width, height, 'black')
 const Density = 100 // measured in kg/pixel
 const ObjArray = []
 let CircleCoords = []
+//object
 const detectCollision = (object, array) =>{
   for(const element of array){
     if (object.radius+ element.radius < Math.hypot(Math.abs(object.x - element.x), Math.abs(object.y - element.y))){
@@ -10,17 +35,22 @@ const detectCollision = (object, array) =>{
   }
 }
 class Shape{
-  constructor(radius, activeForce, coordinates){
+  constructor(radius, activeForce, x,y){
     this.mass = (Math.PI * radius) **2
-
+    this.location = {x,y}
   }
 }
 
-const drawnCircle = (coordArray) => {
-  if (coordArray.length == 2){
+const drawnCircle = (coordArray,) => {
+  if (coordArray.length == 3){
     const radius = Math.hypot(Math.abs(coordArray[0].x-coordArray[1].x),Math.abs(coordArray[0].y-coordArray[1].y))
+    const movementVector = {
+    magnitude: Math.hypot(Math.abs(coordArray[0].x-coordArray[2].x),Math.abs(coordArray[0].y-coordArray[2].y)), 
+    angle: Math.atan2()
+    }
     drawCircle(coordArray[0].x, coordArray[0].y, radius, 'white')
     //drawLine(coordArray[0].x, coordArray[0].y, coordArray[1].x, coordArray[1].y, 1, 'white')
+    ObjArray.push(new Shape(radius, ))
     CircleCoords = []
   }
 }
