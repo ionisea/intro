0c617fb5da0a7ff82c14f3872e3de259549ba658
ion <111394893+ionisea@users.noreply.github.com>
2022-12-26 16:49:24 -0800

    Updating assignments/maze-solver/code.js

--- assignments/maze-solver/code.js
+++ assignments/maze-solver/code.js
@@ -35,7 +35,7 @@ const detectCollisions = (object, array) =>{
   const collisions = []
   for(const element of array){
     const distance = Math.hypot(Math.abs(object.x - element.x), Math.abs(object.y - element.y))
-    if ((object.radius + element.radius > distance) && (distance != 0)){
+    if ((object.radius + element.radius > distance)){
       collisions.push({object: array.findIndex(element), angle: Math.atan2(element.y - object.y, element.x - object.x)})
     }
   }
@@ -48,6 +48,7 @@ class Shape{
     this.x = x
     this.y = y
     this.force = activeForce
+    this.radius = radius
   }
 
 }
