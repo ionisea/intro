ff71e7a96e415eefccbb858c3a321e51b77f0942
ion <111394893+ionisea@users.noreply.github.com>
2022-12-27 09:25:54 -0800

    Updating assignments/maze-solver/code.js

--- assignments/maze-solver/code.js
+++ assignments/maze-solver/code.js
@@ -60,7 +60,7 @@ const evalCollisions = (object) =>{
     returnObject.area += element.source.area
     returnObject.radius = Math.sqrt(object.area)/Math.PI
     returnObject.force.concat(element.force)
-    ObjArray.splice(index,1)
+    ObjArray.splice(element.index,1)
   }
   return returnObject
 }
