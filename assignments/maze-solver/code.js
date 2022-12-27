f1f99913a8edd08d82d2a316b57686954be1669a
ion <111394893+ionisea@users.noreply.github.com>
2022-12-26 16:17:17 -0800

    Updating assignments/maze-solver/code.js

--- assignments/maze-solver/code.js
+++ assignments/maze-solver/code.js
@@ -32,7 +32,7 @@ let CircleCoords = []
 //detectcollision has no use for now
 const detectCollision = (object, array) =>{
   for(const element of array){
-    const objDistance = Math.hypot(Math.abs(object.x - element.x), Math.abs(object.y - element.y))
+    const distance = Math.hypot(Math.abs(object.x - element.x), Math.abs(object.y - element.y))
     if (object.radius+ element.radius > distance && distance != 0){
       return Math.atan2(element.y - object.y, element.x - object.x)
     }
