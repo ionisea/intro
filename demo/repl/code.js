dd718ee172317058b30f1552c3c71c9f49d79d41
ion <111394893+ionisea@users.noreply.github.com>
2023-01-09 15:29:33 -0800

    Updating demo/repl/code.js

--- demo/repl/code.js
+++ demo/repl/code.js
@@ -24,5 +24,5 @@ const dispTest = (vel, angle) =>{
   const h = vel
   const p = Math.cos(angle * Math.PI /180) * h
   const b = Math.sqrt(h**2 - p**2)
-  return {xChange: b, yChange: p}
+  return {xChange: b+10**-15, yChange: p+10**-15}
 }
