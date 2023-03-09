894c5b0829a52040cc39dca01caeb494778b08fb
ionisea <ioncook@students.berkeley.net>
2023-03-09 18:49:18 +0000

    I have no clue on how to fix this. Will return in morning.

--- projects/parallax/public/script.js
+++ projects/parallax/public/script.js
@@ -1,7 +1,7 @@
 import { setCanvas, drawCircle, drawFilledCircle, clear, width, height, animate, now, drawFilledPolygon, drawPolygon, registerOnClick, registerOnKeyDown, randColor, drawFilledRect, } from './graphics.js';
 const canvas = document.getElementById('screen')
 setCanvas(canvas)
-/* used for a physics thing ignore
+/* used for a physics class thing ignore
 const randomizeArrayOrder = arr => {
   const rand = []
   while (rand.length < arr.length) {
@@ -22,12 +22,8 @@ class point {
     this.z = z
   }
 
-  hypot(point, dim1, dim2) { //I may use dims later to compare y axis
-    return Math.hypot(Math.abs(this.x - point.x), Math.abs(this.z - point.z))
-  }
-
-  angle(point, dim1, dim2) { //comment above
-    return Math.atan2(this.x - point.x, point.z)
+  dist3D(point2) {
+    return Math.hypot(Math.abs(this.x - point2.x), Math.abs(this.y - point2.y), Math.abs(this.z - point2.z))
   }
 }
 
@@ -45,11 +41,10 @@ class layer {
 
   drawLayer() {
     const canvPositions = this.vertices.map((p) => {
-      const xAngle = Math.atan2(p.x - camCoords.x, p.z) //camCoords.angle(p)
-      const yAngle = Math.atan2(p.y - camCoords.y, p.z)
+      const xAngle = Math.atan2(p.x - camCoords.x, p.z - camCoords.z) //using minus for zoom in / out
+      const yAngle = Math.atan2(p.y - camCoords.y, p.z - camCoords.z)
       return { x: xAngle * 1200 / Math.PI + width / 2, y: yAngle * 600 / Math.PI + height / 2, z: p.z }
     })
-    //console.log(canvPositions, 'canv pos')
     drawFilledPolygon(canvPositions, this.color)
     drawPolygon(canvPositions, 'black', 1)
   }
@@ -63,15 +58,13 @@ const drawLayerArray = (arr) => {
 }
 
 canvas.onclick = (ev) => {
-  if (!running) {
-    drawFilledCircle(ev.x, ev.y, 2, 'black')
-    const depth = parseInt(document.getElementById('depth').value)
-    const xAngleDiff = (ev.x - width / 2 - camCoords.x) / 1200 * Math.PI // 120 deg
-    const yAngleDiff = (ev.y - height / 2 - camCoords.y) / 600 * Math.PI // 60 deg
-    const trueX = camCoords.x + depth * Math.tan(xAngleDiff) // atan(trueXDistFromCamcoord / depth) to get canvas coord
-    const trueY = camCoords.y + depth * Math.tan(yAngleDiff) // may use when implementing y changes
-    pointArr.push(new point(trueX, trueY, depth))
-  }
+  drawFilledCircle(ev.x, ev.y, 2, 'black')
+  const depth = parseInt(document.getElementById('depth').value)
+  const xAngleDiff = (ev.x - width / 2 - camCoords.x) / width * Math.PI // assuming 10px = 1deg
+  const yAngleDiff = (ev.y - height / 2 - camCoords.y) / height * Math.PI // assuming 10px = 1deg
+  const trueX = camCoords.x + depth * Math.tan(xAngleDiff)
+  const trueY = camCoords.y + depth * Math.tan(yAngleDiff)
+  pointArr.push(new point(trueX, trueY, depth))
 }
 
 document.onkeydown = (k) => {
@@ -84,10 +77,10 @@ document.onkeydown = (k) => {
   } else if (k.key == 'Space') {
     running = !running
   } else if (k.key == 'ArrowRight') {
-    camCoords.x -= 10
+    camCoords.x += 10
     drawLayerArray(layerArray)
   } else if (k.key == 'ArrowLeft') {
-    camCoords.x += 10
+    camCoords.x -= 10
     drawLayerArray(layerArray)
   } else if (k.key == 'ArrowUp') {
     camCoords.y -= 10
@@ -98,8 +91,3 @@ document.onkeydown = (k) => {
   }
 }
 
-animate((t) => {
-  if (running) {
-    drawLayerArray(layerArray)
-  }
-});
