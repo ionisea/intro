a9714572db6d2154f265cf94b9f8dddec3065922
ion <111394893+ionisea@users.noreply.github.com>
2023-04-19 12:35:12 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -5,3 +5,11 @@ const product = (arr) => {
     return arr[0] * product(arr.slice(1))
   }
 }
+
+const sumSquares = n => {
+  if (n === 0) {
+    return 1
+  } else {
+    return n*sumSquares(n-1)
+  }
+}
