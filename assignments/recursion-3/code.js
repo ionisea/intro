014ca3d85d1c4697c260195d2640f4139fe67592
ion <111394893+ionisea@users.noreply.github.com>
2023-04-19 14:58:19 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -54,6 +54,14 @@ const sumNested = (nest) => {
   }
 }
 
+const sumNested = (nest, n) => {
+  if (isNumber(nest)) {
+    return nest
+  } else {
+    return searchNested(nest[0], n) + searchNested(nest.slice(1), n)
+  }
+}
+
 const searchNested = (nest, n) => {
   if (isNumber(nest)) {
     return nest === n
