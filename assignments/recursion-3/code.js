25dd2c763c35cfe106f58b39c472d8db9b666d8a
ion <111394893+ionisea@users.noreply.github.com>
2023-04-20 14:14:53 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -66,5 +66,5 @@ const searchNested = (nest, n) => {
 
 const evaluate = (exp) => {
   if (isNumber(exp)) return exp
-  else return Function(evaluate(exp.left) + exp.op + evaluate(exp.right))
+  else return eval(evaluate(exp.left) + exp.op + evaluate(exp.right))
 }
