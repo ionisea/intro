8be1b9428fe72a95754edf636ba00577f1bda10a
ionisea <ioncook@students.berkeley.net>
2023-04-24 21:41:44 +0000

    further stuff

---
+++ projects/Grapher/public/mathFuncs.js
@@ -0,0 +1,8 @@
+const checkAdjacent = (exp) =>{
+
+}
+
+const mod = (x, m) => ((x%m) +m) %m
+const sin = (t) => Math.sin(t);
+const cos = (t) => Math.cos(t);
+const nrt = (n, rt) => Math.pow(n, 1/rt)
--- projects/Grapher/public/script.js
+++ projects/Grapher/public/script.js
@@ -1,7 +1,9 @@
 import { setCanvas, drawCircle, width, height } from './graphics.js';
 setCanvas(document.getElementById('screen'))
 
-const input = document.getElementById('eq')
+const input = document.getElementById('eq');
+const getCurrentEquation = () => input.value;
+
 
 class Point {
     constructor(x, y) {
@@ -10,5 +12,27 @@ class Point {
     };
 };
 
-let bl = new Point(0,0) //bottom left coord
-let scale = 0.2 //px per unit
+let center = new Point(width / 10, height / 10); //center coord
+let scale = 5; //px per unit
+let resolution = 1; //px between points
+
+
+const reCenter = (mult, newCenter) => {
+    center = newCenter;
+    scale *= mult;
+};
+
+const evaluate = (eq, x) => { //things js cannot understand: 'x(), (x-y)(2), etc' 'trigfunction()' 'a mod (or things like it) b'
+    if (x != undefined) let mutedEq = eq.replaceAll('x', x)
+
+    if (eq.indexOf('(') === -1) {
+        //go into deeper level evaluation, could just use eval func for some of this i suppose
+    } else {
+        return evaluate(eq.substring(eq.indexOf('('), eq.indexOf(')')))
+    }
+}
+
+const graph = (eq, x) =>{
+    const accumulate = []
+   // for (let x = (center-width/2); x < )
+}
