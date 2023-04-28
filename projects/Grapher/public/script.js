0517cbd2b02da42036947b0954ab124fadc78e40
ion <111394893+ionisea@users.noreply.github.com>
2023-04-28 22:10:37 +0000

    progress (nothing in the readme yet)

--- projects/Grapher/public/script.js
+++ projects/Grapher/public/script.js
@@ -1,5 +1,5 @@
 import { setCanvas, drawCircle, width, height } from './graphics.js';
-import {maths} from './mathFuncs.js';
+import { maths } from './mathFuncs.js';
 
 setCanvas(document.getElementById('screen'))
 const canvas = document.getElementById('screen')
@@ -7,10 +7,10 @@ const input = document.getElementById('eq');
 
 const getElementValue = (e) => e.value;
 
-const crashPage = () =>{ // I may use this in case some jerk tries to exploit security vulnerabilities that I may or may not have
+const crashPage = () => { // I may use this in case some jerk tries to exploit security vulnerabilities that I may or may not have
     while (true) {
         setTimeout(() => {
-            console.log(maths[Math.random()* maths.values().length](Math.random(), Math.random))
+            console.log(maths[Math.random() * maths.values().length](Math.random(), Math.random))
         }, 10)
     }
 }
@@ -21,7 +21,7 @@ class Point {
         this.y = y;
     };
 
-    translate(dir){
+    translate(dir) {
 
     }
 };
@@ -30,34 +30,40 @@ let center = new Point(width / 10, height / 10); //center coord
 let scale = 5; //px per unit
 let resolution = 1; //px between points
 
-const getCanvBL = (ctr) =>  new Point(ctr - width/2/scale, ctr - height/2/scale)
+const getCanvBL = (ctr) => new Point(ctr - width / 2 / scale, ctr - height / 2 / scale)
 
 const reCenter = (mult, newCenter) => { //for later zoom in/out
     center = newCenter;
     scale *= mult;
 };
 
-const checkAdjacent = (exp) =>{
-
+const fixAdjacent = (exp) => {
+    let newExp = ''
+    for (let x = 0; x < exp.length - 1; x++) {
+        //the reason the following two are not bundled into one is because both of them may trigger
+        if ((parseInt(exp[x], 10) !== NaN) && (newExp[newExp[newExp.length - 1]] === ')')) newExp += '*'
+        if ((parseInt(newExp[newExp.length - 1], 10) !== NaN) && (exp[x] === '(')) newExp += '*'
+        newExp += exp[x]
+    }
+    return newExp
 }
 
 const evaluate = (eq, x) => { //things js cannot understand: 'x(), (x-y)(2), etc' 'trigfunction()' '|num|' 'a mod (or things like it) b' 'num!'
-    if (x != undefined) evaluate(eq.replaceAll('x', `(${x})`).replaceAll(' ', ''))
+    if (x != undefined) evaluate(fixAdjacent(eq).replaceAll('x', `(${x})`).replaceAll(' ', ''))
     if (eq.indexOf('(') === -1) {
         //go into deeper level evaluation, could just use eval func for some of this i suppose
     } else {
-
-        return evaluate(eq.substring(eq.indexOf('(')+1, eq.indexOf(')')-1))
+        return evaluate(eq.substring(eq.indexOf('(') + 1, eq.indexOf(')') - 1))
     }
 }
 
 canvas.onclick = (ev) => {
-    reCenter(ev.button === 0 ? 0.5 : 2, )
+    reCenter(ev.button === 0 ? 0.5 : 2, new Point(ev.x, ev.y))
 }
 
-const graph = (eq, x) =>{
+const graph = (eq, x) => {
     const accumulate = []
-    for (let x = (getCanvBL(center)); x < center.x + width/2 * (1/scale); x++){
+    for (let x = 0; x < width; x += resolution) {
 
     }
 }
